1a1ecdfb693daf1f6424720e3f34532228a91325
priyapriyam <priyam18@navgurukul.org>
2019-06-10 11:01:49 +0530

    in this question i have to do all the multiply of dictionary keys

---
+++ dict_multiply.py
@@ -0,0 +1,10 @@
+students={"orange":9,"pineapple":8,"mangoes":7,"apple":3,"grapesh":4}
+a= (students.values())
+print a
+i=0
+multiply=1
+while i<len(a):
+    multiply=multiply*a[i]
+    i=i+1
+print multiply
+    
